873c7382d4d54877c9529e18a45e926d21370c9f
Lisardo <lisardo.kist@gmail.com>
2016-07-14 19:16:44 -0300

    Kotlin and basic test

---
+++ app/src/test/java/com/example/lisardo/tacoapplication/TacoUnitTest.kt
@@ -0,0 +1,18 @@
+package com.example.lisardo.tacoapplication
+
+import android.text.format.DateUtils
+import org.junit.Test
+import java.util.Date
+import org.junit.Assert.*
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+class TacoUnitTest {
+    @Throws(Exception::class)
+    fun today_isCorrect() {
+        val date = Date()
+        val today = DateUtils.isToday(date.time)
+        assertTrue(today)
+    }
+}/**/
