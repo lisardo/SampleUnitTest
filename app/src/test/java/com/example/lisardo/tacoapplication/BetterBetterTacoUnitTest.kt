df2debad3535c448c6ac36edbf7cb61d744e05cf
Lisardo <lisardo.kist@gmail.com>
2016-07-15 09:01:11 -0300

    Improve Roboeletric test.

--- app/src/test/java/com/example/lisardo/tacoapplication/BetterBetterTacoUnitTest.kt
+++ app/src/test/java/com/example/lisardo/tacoapplication/BetterBetterTacoUnitTest.kt
@@ -1,10 +1,12 @@
 package com.example.lisardo.tacoapplication
 
+import android.app.Activity
 import android.text.format.DateUtils
 import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mockito
+import org.robolectric.Robolectric
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
 import java.util.*
@@ -15,9 +17,13 @@ class BetterBetterTacoUnitTest {
 
     @Test
     fun today_isCorrect() {
-        val date = Date()
-        val tacoDay = DateUtils.isToday(date.time)
-        assert(tacoDay)
+        val activity = Robolectric.setupActivity(TacoActivity::class.java)
+        val text = TacoText(activity.baseContext).getText()
+        assertEquals(text, FAKE_STRING)
+    }
+
+    companion object {
+        private val FAKE_STRING = "Hello World"
     }
 
 }
